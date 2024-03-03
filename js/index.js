e575b6836f33772659bb7aa01e3319040b75dfcb
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-03-03 17:38:04 +0600

    latest posts section in added javascript

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const allPostDisplay = (posts) => {
     // console.log(posts);
     const postContainer = document.getElementById('post-container');
     posts.forEach(post => {
-        console.log(post);
+        // console.log(post);
         const div = document.createElement('div');
         // div.classList.add(`w-full md:w-3/5 flex gap-4 bg-[#F3F3F5] p-6 rounded-3xl`);
         div.innerHTML = `
@@ -26,26 +26,26 @@ const allPostDisplay = (posts) => {
                         <p class="text-sm text-[#12132DCC] font-bold">Author: <span>${post?.author?.name}</span></p>
                     </div>
                     <div class="pb-6 border-b-2 border-dashed space-y-3">
-                        <h2 class="text-xl font-bold">${post.title}</h2>
-                        <p class="text-[#12132D99] text-lg">${post.description}</p>
+                        <h2 class="text-xl font-bold">${post?.title}</h2>
+                        <p class="text-[#12132D99] text-lg">${post?.description}</p>
                     </div>
                     <div class="pt-6 flex justify-between items-center md:gap-5 gap-2">
                         <div class="flex gap-4">
                             <span class="flex justify-center items-center gap-2">
                                 <img src="./images/comment.png" alt="">
-                                <p>${post.comment_count}</p>
+                                <p>${post?.comment_count}</p>
                             </span>
                             <span class="flex justify-center items-center gap-2">
                                 <img src="./images/view.png" alt="">
-                                <p>${post.view_count}</p>
+                                <p>${post?.view_count}</p>
                             </span>
                             <span class="flex justify-center items-center gap-2">
                                 <img src="./images/time.png" alt="">
-                                <p class="text-nowrap">${post.posted_time}</p>
+                                <p class="text-nowrap">${post?.posted_time}</p>
                             </span>
                         </div>
                         <div>
-                            <button onclick="readBtn(${post.id})"><img src="./images/mes-box.png" alt=""></button>
+                            <button onclick="readBtn(${post?.id})"><img src="./images/mes-box.png" alt=""></button>
                         </div>
                     </div>
                 </div>
@@ -68,10 +68,28 @@ const readBtn = async (id) => {
 const latestPosts = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
     const data = await res.json();
-    console.log(data)
+    // console.log(data)
+    const lMainContainer = document.getElementById('l-main-container')
+    const latestContainer = document.getElementById('latest-container');
     data.forEach(item => {
         console.log(item)
-        
+
+        const div = document.createElement('div');
+        div.classList.add('rounded-3xl', 'border-2', 'space-y-4', 'p-6');
+        div.innerHTML = `
+        <img class="w-full" src="${item?.cover_image}" alt="">
+                <h3 class="flex gap-3"><img src="./images/cal.png" alt=""><p class="text-[#12132D99]">${item?.author?.posted_date??"No publish date"}</p></h3>
+                <h1 class="text-lg font-extrabold">${item?.title}</h1>
+                <p class="text-base text-[#12132D99]">${item?.description}</p>
+                <div class="flex gap-4">
+                    <img class="w-16 rounded-full" src="${item?.profile_image}" alt="">
+                    <div class="flex flex-col">
+                        <h2 class="text-base font-bold">${item?.author?.name}</h2>
+                        <p class="text-[#12132D99]">${item?.author?.designation??"Unknown"}</p>
+                    </div>
+                </div>
+        `;
+        latestContainer.appendChild(div);
     })
 }
 
